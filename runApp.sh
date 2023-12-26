f65c69e951045c33d0eb99fe22d069e8b00e5dad
colinaudergon <63097004+colinaudergon@users.noreply.github.com>
2023-12-26 15:18:17 +0100

    Update runApp.sh

--- runApp.sh
+++ runApp.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-# replace absolute path with true application location 
+# Replace absolute path with the true application location 
 cd Documents/flask_api_embedded_linux
 current_dir=$(pwd)
 echo "Current directory: $current_dir"
 
+pathToPythonScript="app.py"
+pathToCprogram="app.c"
 
-pathToPythonScript=""
-pathToCprogramm=""
-#compile C programm ?
-#error handling if C prog isnt compiled
- #install python dependency
+# Compile C program
+gcc $pathToCprogram -o app_executable
+if [ $? -ne 0 ]; then
+    echo "Error: Compilation of C program failed."
+    exit 1
+else
+    echo "C program compiled successfully."
+fi
 
-#Run python programm
-# python $pathToPythonScript
-#run c programm
+# Install Python dependencies
+# Add your command for installing Python dependencies here
+
+# Run Python program
+python $pathToPythonScript
+
+# Run compiled C program
+./app_executable
