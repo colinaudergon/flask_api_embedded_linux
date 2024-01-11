ceb4867572e5af94dda95adf78ff7ec07b5b43f8
colinaudergon <63097004+colinaudergon@users.noreply.github.com>
2024-01-11 16:03:47 +0100

    Update imageProcTestRun.sh

--- imageProc/imageProcTestRun.sh
+++ imageProc/imageProcTestRun.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-# Replace absolute path with the true application location 
-#include dependency
+# Replace absolute path with the true application location
+# Include dependency
 cd ..
 current_dir=$(pwd)
 echo "Current directory: $current_dir"
 
 cmd="git pull"
 echo $cmd
+git stash # Stash local changes before pulling
+git pull
+git stash pop # Apply stashed changes
+
 cd "imageProc"
 
 pathToPythonScript="imageProc.py"
-pathToCprogram="mainWithArg.c"
-# Add correct path to screen proogramm
-# pathToScreenProgram="app.c"
+pathToCprogram=$(realpath mainWithArg.c)  # Full path to the C program
 
 # Compile C program
 gcc $pathToCprogram -o frameBufferHandler
@@ -22,18 +24,13 @@ if [ $? -ne 0 ]; then
 else
     echo "C program compiled successfully."
 fi
-# Compile C frameBufferHandler programm
-# gcc $pathToCprogram -o frameBufferHandler
-# if [ $? -ne 0 ]; then
-#     echo "Error: Compilation of frameBufferHandler program failed."
-#     exit 1
-# else
-#     echo "frameBufferHandler program compiled successfully."
-# fi
 
 # Install Python dependencies
 # Add your command for installing Python dependencies here
 
-# Run Python program
+# Run Python script
 echo "Running python script."
-python $pathToPythonScript
+python3 $pathToPythonScript  # Use python3 for Python 3
+
+# Give execution rights to the script
+chmod +x imageProcTestRun.sh
