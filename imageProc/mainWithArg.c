80808d454c9d4dbac774f7ed74306a0511c45d20
colinaudergon <63097004+colinaudergon@users.noreply.github.com>
2024-01-11 15:31:38 +0100

    Update mainWithArg.c

--- imageProc/mainWithArg.c
+++ imageProc/mainWithArg.c
@@ -166,9 +166,9 @@ int main(int argc, char *argv[])
       }
     }
 
-  for (y = 0; y < height; y++)
+  for (int y = 0; y < height; y++)
 {
-    for (x = 0; x < width; x++)
+    for (int x = 0; x < width; x++)
     {
         // Access RGB values from the file
         struct RGB_COLOR rgbValue;
