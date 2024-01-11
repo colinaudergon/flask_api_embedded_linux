7751f28397ee599bcec4c684d3e2e3c58321cf4e
colinaudergon <63097004+colinaudergon@users.noreply.github.com>
2024-01-11 15:35:43 +0100

    Update mainWithArg.c

--- imageProc/mainWithArg.c
+++ imageProc/mainWithArg.c
@@ -162,7 +162,7 @@ int main(int argc, char *argv[])
   {
     for (w = 0; w < fbVarScreenInfo.xres; w++)
     {
-      pfb32[w + h * fbVarScreenInfo.xres] = BLACK;
+      pfb32[w + h * fbVarScreenInfo.xres] = BLUE;
     }
   }
 
