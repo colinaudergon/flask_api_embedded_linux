a99b87c030cd96e216b2140b5493e6eacab9da8f
colinaudergon <63097004+colinaudergon@users.noreply.github.com>
2024-01-11 15:51:42 +0100

    Update imageProc.py

--- imageProc/imageProc.py
+++ imageProc/imageProc.py
@@ -117,7 +117,7 @@ text= f"IP ADDRESS: {ip}\n"
 # # ~980 character max
 fb_width = 1920  # Set this to your framebuffer width
 fb_height = 1080  # Set this to your framebuffer height
-(display,displayArr) = improc.createImage(text,fb_width,fb_height,fontSize,fb_width/2,fb_height/2)
+(display,displayArr) = improc.createImage(text,fb_width,fb_height,fontSize,10,10)
 improc.transmitArrayToCframeBufferHandler(displayArr)
 # display.save("ipaddress.png")
 print(displayArr.ndim)
