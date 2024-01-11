293c276a03504582cfa0bfc67f9b2b8870300d1b
colinaudergon <63097004+colinaudergon@users.noreply.github.com>
2024-01-11 15:19:45 +0100

    Update mainWithArg.c

--- imageProc/mainWithArg.c
+++ imageProc/mainWithArg.c
@@ -148,7 +148,8 @@ int main(int argc, char *argv[])
   }
   int32_t screensize = (fbVarScreenInfo.yres_virtual * fbVarScreenInfo.xres_virtual * fbVarScreenInfo.bits_per_pixel) / 8;
   int32_t *pfb32 = (int32_t *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, 0);
-  printf("Screensize: %d\npfb32: %d\n",screensize,&pfb32);
+  int32_t temp=&pfb32;
+  printf("Screensize: %d\npfb32: %d\n",screensize,temp);
   // int32_t *pfb32 = (int32_t *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, 0);
   if (pfb32 == (int32_t *)-1)
   {
