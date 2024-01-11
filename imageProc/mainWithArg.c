d4f8a311f309336b276e6a6001227c97c30036e5
colinaudergon <63097004+colinaudergon@users.noreply.github.com>
2024-01-11 15:32:22 +0100

    Update mainWithArg.c

--- imageProc/mainWithArg.c
+++ imageProc/mainWithArg.c
@@ -158,40 +158,40 @@ int main(int argc, char *argv[])
   }
 
   /* Fill the screen with 32 bpp, do it for all [x,y] pixel with desired color */
-    for (h = 0; h < fbVarScreenInfo.yres; h++)
+  for (h = 0; h < fbVarScreenInfo.yres; h++)
+  {
+    for (w = 0; w < fbVarScreenInfo.xres; w++)
     {
-      for (w = 0; w < fbVarScreenInfo.xres; w++)
+      pfb32[w + h * fbVarScreenInfo.xres] = BLACK;
+    }
+  }
+
+  for (int x = 0; x < width; x++)
+  {
+    for (int y = 0; y < height; y++)
+    {
+      // Access RGB values from the file
+      struct RGB_COLOR rgbValue;
+      if (fscanf(inputFile, "%hhu %hhu %hhu", &rgbValue.r, &rgbValue.g, &rgbValue.b) != 3)
       {
-        pfb32[w + h * fbVarScreenInfo.xres] = BLACK;
+        fprintf(stderr, "Error reading RGB values from input file\n");
+        fclose(inputFile);
+        free(pfb_rgb);
+        return 1;
       }
+
+      // Swap x and y for 90 degrees clockwise rotation
+      uint32_t pixel_pos = y + x * fbVarScreenInfo.xres_virtual;
+
+      // Assuming 24-bit framebuffer, pack RGB values into a single 32-bit pixel
+      uint32_t pixel = (rgbValue.r << 16) | (rgbValue.g << 8) | rgbValue.b;
+
+      // Copy pixel to the framebuffer
+      pfb_rgb[pixel_pos].r = rgbValue.r;
+      pfb_rgb[pixel_pos].g = rgbValue.g;
+      pfb_rgb[pixel_pos].b = rgbValue.b;
     }
-
-  for (int y = 0; y < height; y++)
-{
-    for (int x = 0; x < width; x++)
-    {
-        // Access RGB values from the file
-        struct RGB_COLOR rgbValue;
-        if (fscanf(inputFile, "%hhu %hhu %hhu", &rgbValue.r, &rgbValue.g, &rgbValue.b) != 3)
-        {
-            fprintf(stderr, "Error reading RGB values from input file\n");
-            fclose(inputFile);
-            free(pfb_rgb);
-            return 1;
-        }
-
-        // Swap x and y for 90 degrees clockwise rotation
-        uint32_t pixel_pos = y + x * fbVarScreenInfo.xres_virtual;
-
-        // Assuming 24-bit framebuffer, pack RGB values into a single 32-bit pixel
-        uint32_t pixel = (rgbValue.r << 16) | (rgbValue.g << 8) | rgbValue.b;
-
-        // Copy pixel to the framebuffer
-        pfb_rgb[pixel_pos].r = rgbValue.r;
-        pfb_rgb[pixel_pos].g = rgbValue.g;
-        pfb_rgb[pixel_pos].b = rgbValue.b;
-    }
-}
+  }
 
   /* Cleanup */
   free(pfb_rgb);
