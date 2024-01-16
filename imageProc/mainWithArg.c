97596ccbd37f17efc2a387049e95acda7039d70a
colinaudergon <63097004+colinaudergon@users.noreply.github.com>
2024-01-16 09:19:11 +0100

    Update mainWithArg.c

--- imageProc/mainWithArg.c
+++ imageProc/mainWithArg.c
@@ -171,9 +171,9 @@ int main(int argc, char *argv[])
     //   }
     // }
 
-    for (w = 0; w < fbVarScreenInfo.xres; w++)
+    for (h = 0; h < fbVarScreenInfo.yres; h++)
     {
-      for (h = 0; h < fbVarScreenInfo.yres; h++)
+      for (w = 0; w < fbVarScreenInfo.xres; w++)
       {
         struct RGB_COLOR rgbValue;
         if (fscanf(inputFile, "%hhu %hhu %hhu", &rgbValue.r, &rgbValue.g, &rgbValue.b) != 3)
@@ -183,7 +183,7 @@ int main(int argc, char *argv[])
           free(pfb_rgb);
           return 1;
         }
-        pfb32[w + h * fbVarScreenInfo.xres] = CONVERT_RGB24(rgbValue.r, rgbValue.g, rgbValue.b);
+        pfb32[h + w * fbVarScreenInfo.yres] = CONVERT_RGB24(rgbValue.r, rgbValue.g, rgbValue.b);
       }
       // int32_t pos = h + w * fbVarScreenInfo.xres;
       // printf("PosX: %d; PosY: %d\nPosTot\n\n: %d", w, h, pos);
