0a83403133e8ee04a1a63705270d96c6779deb5d
colinaudergon <63097004+colinaudergon@users.noreply.github.com>
2024-01-16 08:48:29 +0100

    Update imageProc.py

--- imageProc/imageProc.py
+++ imageProc/imageProc.py
@@ -112,7 +112,7 @@ class ImageProcessor():
         # Rotate the imageArray 90° clockwise
         rotated_imageArray = np.rot90(imageArray, k=-1, axes=(0, 1))
         # Rotate the imageArray 90° clockwise
-        # rotated_imageArray1 = np.rot90(rotated_imageArray, k=-1, axes=(0, 1))
+        rotated_imageArray1 = np.rot90(rotated_imageArray, k=-1, axes=(0, 1))
         # # Rotate the imageArray 90° clockwise
         # rotated_imageArray2 = np.rot90(rotated_imageArray1, k=-1, axes=(0, 1))
         # rotated_imageArray2 = np.rot90(rotated_imageArray1, k=-1, axes=(0, 1))
@@ -120,7 +120,7 @@ class ImageProcessor():
         print(f"Rotated image size - height:{height}, width: {width}, channels: {_}")
 
         # Flatten the RGB values into a 1D array
-        flat_array = rotated_imageArray.reshape(-1)
+        flat_array = rotated_imageArray1.reshape(-1)
         print(f"Flat array: {flat_array}")
 
         # Create a temporary file to store the RGB values
